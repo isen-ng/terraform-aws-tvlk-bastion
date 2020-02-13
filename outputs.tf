f71d4062ad520a5c705fc8a05660dcce3ed5ea75
Isen Ng <3904043+isen-ng@users.noreply.github.com>
2020-02-13 13:31:11 +0800

    useless

--- outputs.tf
+++ outputs.tf
@@ -8,30 +8,30 @@ output "sg_bastion_id" {
   value       = "${aws_security_group.bastion.id}"
 }
 
-output "shared_sg_postgres_id" {
-  description = "id of shared security group for postgres."
-  value       = "${aws_security_group.postgres.id}"
-}
+# output "shared_sg_postgres_id" {
+#   description = "id of shared security group for postgres."
+#   value       = "${aws_security_group.postgres.id}"
+# }
 
-output "shared_sg_mongod_id" {
-  description = "id of shared security group for mongod."
-  value       = "${aws_security_group.mongod.id}"
-}
+# output "shared_sg_mongod_id" {
+#   description = "id of shared security group for mongod."
+#   value       = "${aws_security_group.mongod.id}"
+# }
 
-output "shared_sg_mysql_id" {
-  description = "id of shared security group for mysql."
-  value       = "${aws_security_group.mysql.id}"
-}
+# output "shared_sg_mysql_id" {
+#   description = "id of shared security group for mysql."
+#   value       = "${aws_security_group.mysql.id}"
+# }
 
-output "shared_sg_memcached_id" {
-  description = "id of shared security group for memcached."
-  value       = "${aws_security_group.memcached.id}"
-}
+# output "shared_sg_memcached_id" {
+#   description = "id of shared security group for memcached."
+#   value       = "${aws_security_group.memcached.id}"
+# }
 
-output "shared_sg_redis_id" {
-  description = "id of shared security group for redis."
-  value       = "${aws_security_group.redis.id}"
-}
+# output "shared_sg_redis_id" {
+#   description = "id of shared security group for redis."
+#   value       = "${aws_security_group.redis.id}"
+# }
 
 output "instance_role_name" {
   description = "role name for the instances."
