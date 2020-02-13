4b1e8978ddf35b1a99afa3edae6cfcf09c0db075
Isen Ng <3904043+isen-ng@users.noreply.github.com>
2020-02-13 13:24:24 +0800

    ???2

--- data.tf
+++ data.tf
@@ -11,16 +11,6 @@ data "aws_subnet_ids" "subnet" {
   }
 }
 
-data "aws_ami" "bastion_ami" {
-  most_recent = "true"
-  owners      = ["${var.ami_owner_account_id}"]
-
-  filter = {
-    name   = "name"
-    values = ["${var.ami_name_prefix}"]
-  }
-}
-
 data "aws_subnet_ids" "app" {
   vpc_id = "${var.vpc_id}"
 
